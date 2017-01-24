3950ec4c5137bee85dedd9aee39edb14fcbcc007
Kim Phillips <kim.phillips@arm.com>
2017-01-24 14:23:08 -0600

    Wednesdays are reserved

--- lunch.sh
+++ lunch.sh
@@ -10,8 +10,11 @@ Torchy's
 __EOF
 LENGTH=`wc -l lunch-places.txt | cut -d\  -f 1`
 WHICHONE=$(($RANDOM * $LENGTH / 32768 + 1))
-head -$WHICHONE lunch-places.txt | tail -1
+if [[ $(date +%u) -eq 3 ]] ; then
+	echo "Taco Wednesday @ Doc's Backyard"
+else
+	head -$WHICHONE lunch-places.txt | tail -1
+fi
 rm lunch-places.txt
 echo -n jlinton\'s version:
 curl -v http://www.restaurantgenerator.com/restaurantselect.html?q=5707%20Southwest%20Pkwy%20%23100%2C%20Austin%2C%20TX%2078735\&cheap=cheap 2>/dev/null |grep "infowindow\.setContent" |cut -d\> -f 3 | cut -d\< -f 1
-
